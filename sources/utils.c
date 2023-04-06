c5911ac60c6ebd82aecd4301c53ce4dd3f1efa0e
Sangmin Seo <saseo@c3r1s4.42seoul.kr>
2023-04-06 16:43:43 +0900

    feat : add lexer functions for build list form a line

--- sources/utils.c
+++ sources/utils.c
@@ -1,7 +1,30 @@
 #include "utils.h"
 
+//test main function
+#include <stdio.h>
+
+/*
+int	main(int argc, char **argv, char **envp)
+{
+	char	*line = "\"ls -al $file\"|'cat' '$file'>out|nocmd \"\'$var2\'\"";
+	t_list	*list;
+	int		idx = 0;
+
+	printf("%s\n", line);
+	list = lexer(line, envp);
+	while (list)
+	{
+		printf("[%s]-", list->line);
+		idx++;
+		list = list->next;
+	}
+	printf("\n");
+	return 0;
+}
+*/
+
 //Not Implemeted yet
-t_tree	*get_tree(char *line)
+t_tree	*get_tree(char *line, char **envp)
 {
 	t_tree	*tree;
 	t_list	*list;
@@ -11,9 +34,43 @@ t_tree	*get_tree(char *line)
 	// tree builder		: return pointer of tree
 	//					: each level call their lower process functs recursively
 
-	//list = get_lexica(line);
+	list = lexer(line, envp);//need to implement
+	if (!syntax(&list))
+		return (NULL);
 	tree = tree_builder(list);
-	if (!tree)
-		free_tree(tree);
 	return (tree);
+}
+
+int	syntax(t_list **head)
+{
+	int	ret;
+
+	ret = 1;
+	free_line_list(*head);//need to implement
+	line_syn_err();//need to implement
+	return (ret);
+}
+
+void	line_syn_err(void)
+{
+	char	*msg;
+
+	msg = "minishell: line has syntax error!\n";
+	g_last_exit_code = 2;
+	write(2, msg, ft_strlen(msg));
+}
+
+void	free_line_list(t_list *head)
+{
+	t_list	*cur;
+	t_list	*tmp;
+
+	cur = head;
+	while (cur)
+	{
+		free(cur->line);
+		tmp = cur;
+		cur = cur->next;
+		free(tmp);
+	}
 }
---
+++ sources/utils_lexer.c
@@ -0,0 +1,117 @@
+#include "utils.h"
+
+t_list	*lexer(char *line, char **envp)
+{
+	t_list	*ret;
+	char	*replace_line;
+
+	replace_line = get_line_replace_envp(line, envp);
+	//작업..
+	ret = NULL;
+	set_list_from_line(&ret, replace_line);
+	free(replace_line);
+	if (!ret)
+		exit(0);
+	return (ret);
+}
+
+void	set_list_from_line(t_list **head, char *line)
+{
+	char	*tmp;
+	int		token_len;
+
+	tmp = line;
+	token_len = 0;
+	while (*tmp)
+	{
+		if (*tmp == '<' || *tmp == '>')
+			token_len = add_redir(head, tmp);
+		else if (*tmp == '|')
+			token_len = add_pipe(head, tmp);
+		else if (*tmp == '\'' || *tmp == '\"')
+			token_len = add_quotes(head, tmp);
+		else
+			token_len = add_word(head, tmp);
+		tmp += token_len;
+	}
+}
+
+int	add_redir(t_list **head, char *line)
+{
+	t_list	*new;
+	int		len;
+
+	if (*line == '<' && *(line + 1) != '<')
+		new = ft_lstnew(L_REDIR, "<");
+	else if (*line == '<' && *(line + 1) == '<')
+		new = ft_lstnew(L_REDIR, "<<");
+	else if (*line == '>' && *(line + 1) != '>')
+		new = ft_lstnew(L_REDIR, ">");
+	else if (*line == '>' && *(line + 1) == '>')
+		new = ft_lstnew(L_REDIR, ">>");
+	if (!new)
+		exit(0);
+	ft_lstadd_back(head, new);
+	len = ft_strlen(new->line);
+	return (len);
+}
+
+int	add_pipe(t_list **head, char *line)
+{
+	t_list	*new;
+
+	new = ft_lstnew(L_PIPE, "|");
+	if (!new)
+		exit(0);
+	ft_lstadd_back(head, new);
+	return (1);
+}
+
+int	add_quotes(t_list **head, char *line)
+{
+	t_list	*new;
+	char	*new_line;
+	char	quote_type;
+	int		len;
+
+	len = 1;
+	quote_type = *line;
+	while (line[len] != quote_type && line[len] != '\0')
+		len++;
+	new_line = ft_substr(line + 1, 0, len - 1);
+	if (!new_line)
+		exit(0);
+	new = ft_lstnew(L_WORD, new_line);
+	if (!new)
+		exit(0);
+	ft_lstadd_back(head, new);
+	len++;
+	return (len);
+}
+
+int	add_word(t_list **head, char *line)
+{
+	t_list	*new;
+	char	*new_line;
+	int		len;
+
+	len = 0;
+	while (line[len])
+	{
+		if (line[len] == ' ' || (line[len] > 8 && line[len] < 14))
+			break ;
+		if (line[len] == '<' || line[len] == '>' || line[len] == '|')
+			break ;
+		len++;
+	}
+	if (len == 0)
+		return (1);
+	new_line = ft_substr(line, 0, len);
+	if (!new_line)
+		exit(0);
+	new = ft_lstnew(L_WORD, new_line);
+	if (!new)
+		exit(0);
+	ft_lstadd_back(head, new);
+	return (len);
+}
