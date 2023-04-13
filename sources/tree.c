a67c9a7b0a7779741d08854258696d738fc2528a
Sangmin Seo <saseo@c3r2s2.42seoul.kr>
2023-04-13 20:10:02 +0900

    normfix : tree.c, add tree_2.c

--- sources/tree.c
+++ sources/tree.c
@@ -25,13 +25,6 @@ t_node	*create_node(t_token type)
 	return (node);
 }
 
-void	tree_set_root(t_tree *tree, t_node *root)
-{
-	if (!tree)
-		return ;
-	tree->root = root;
-}
-
 void	insert_left(t_node *parent, t_node *child)
 {
 	parent->left = child;
@@ -57,54 +50,4 @@ int	traverse(t_tree *tree, t_node *cur, char ***envp)
 	traverse(tree, cur->left, envp);
 	traverse(tree, cur->right, envp);
 	return (0);
-}
-
-void	free_tree(t_tree *tree)
-{
-	t_node	*node;
-	t_node	*tmp;
-
-	if (!tree)
-		return ;
-	node = tree->root;
-	if (tree->root == NULL)
-	{
-		free(tree);
-		return ;
-	}
-	free_tree_node(node);
-	free(tree);
-}
-
-void	free_tree_node(t_node *node)
-{
-	if (node == NULL)
-		return ;
-	free_tree_node(node->left);
-	free_tree_node(node->right);
-	free_node_type(node);
-	free(node);
-}
-
-void	free_node_type(t_node *node)
-{
-	if (node->type == REDIR)
-		free(node->cont.file_name);
-	else if (node->type == SIMPLE_CMD)
-		free_node_simple_cmd(node);
-	node = NULL;
-}
-
-void	free_node_simple_cmd(t_node *node)
-{
-	char	**tmp;
-
-	tmp = node->cont.args;
-	free(node->cont.path);
-	while (*tmp)
-	{
-		free(*tmp);
-		tmp++;
-	}
-	free(node->cont.args);
 }
