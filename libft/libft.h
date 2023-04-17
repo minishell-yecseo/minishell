6f2bdca34e2c098acb8ed8896edce969dbbb8347
Nam Yechan <chanmias0318@gmail.com>
2023-04-17 16:15:11 +0900

    norm : libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -1,14 +1,14 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
 
 typedef struct s_stack_node
 {
-	char			quote;
+	char				quote;
 	struct s_stack_node	*prev;
 	struct s_stack_node	*next;
 }		t_stack_node;
@@ -16,27 +16,27 @@ typedef struct s_stack_node
 typedef struct s_stack
 {
 	struct s_stack_node	*top;
-	int			size;
+	int					size;
 }		t_stack;
 
-void		*ft_calloc(size_t count, size_t size);
-void		ft_bzero(void *s, size_t n);
-char		**ft_split(char const *s, char c);
-int			ft_isalpha(int c);
-int			ft_isdigit(int c);
+void			*ft_calloc(size_t count, size_t size);
+void			ft_bzero(void *s, size_t n);
+char			**ft_split(char const *s, char c);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
 
-int			ft_strlen(const char *str);
-char		*ft_strstr(char *str, char *to_find);
-char		*ft_strchr(const char *s, int c);
-char		*ft_substr(char const *s, unsigned int start, size_t len);
-char		*ft_strdup(const char *s1);
-char		*ft_strjoin(char const *s1, char const *s2);
-int			ft_memcmp(const void *s1, const void *s2, size_t n);
-int			ft_strcmp(char *s1, char *s2);
-void		ft_strcpy(char *dst, const char *src);
-void		ft_strncpy(char *dest, const char *src, size_t len);
-size_t		ft_strlcpy(char *dest, const char *src, size_t destsize);
-char		*ft_itoa(int n);
+int				ft_strlen(const char *str);
+char			*ft_strstr(char *str, char *to_find);
+char			*ft_strchr(const char *s, int c);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strdup(const char *s1);
+char			*ft_strjoin(char const *s1, char const *s2);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+int				ft_strcmp(char *s1, char *s2);
+void			ft_strcpy(char *dst, const char *src);
+void			ft_strncpy(char *dest, const char *src, size_t len);
+size_t			ft_strlcpy(char *dest, const char *src, size_t destsize);
+char			*ft_itoa(int n);
 
 t_stack			*stack_init(void);
 t_stack_node	*ft_new_stack_node(char quote);
